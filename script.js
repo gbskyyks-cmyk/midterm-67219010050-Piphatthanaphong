5725bb2736857a04fd1362fe11321c40e14b88a0
gbskyyks-cmyk <gbskyyks@gmail.com>
2026-07-15 14:19:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// ดักจับเหตุการณ์เมื่อผู้ใช้คลิกปุ่มที่มี id เป็น alertBtn
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('ใครกดขอให้ได้ ให้ไดแฟนสวยหล่อๆ สาธุบุญ');
+});
